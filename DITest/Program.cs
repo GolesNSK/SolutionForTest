ebce505c414e1eb97a0aaebec40d6f4475251385
Pavel Goleshchikhin <goleshchikhin@yandex.ru>
2021-09-09 23:12:54 +0700

    Добавьте файлы проекта.

---
+++ DITest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace DITest
+{
+    class Program
+    {
+        private const int EXITCODE_OK = 0x0;
+        private const int EXITCODE_ERROR = 0x1;
+
+        static int Main(string[] args)
+        {
+            // Serilog.Extensions.Hosting
+            // Serilog.Sinks.Console
+            // Serilog.Sinks.File
+            // Serilog.Settings.AppSettings
+            Log.Logger = new LoggerConfiguration()
+                         .ReadFrom.AppSettings()
+                         .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+                CreateHostBuilder(args).Build().Run();
+                return EXITCODE_OK;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return EXITCODE_ERROR;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseSerilog();
+    }
+}
---
+++ DITest/Worker.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DITest
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Log.Verbose("Verbose message.");
+            Log.Debug("Debug message.");
+            Log.Information("Information message.");
+            Log.Warning("Warning message.");
+            Log.Error("Error message.");
+            Log.Fatal("Fatal message.");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
